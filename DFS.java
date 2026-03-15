7b59ffd7b7e25579b1682c69bae8345e7ff0f21c
smit659 <75824914+smit659@users.noreply.github.com>
2026-03-15 12:18:55 +0000

    i

--- DFS.java
+++ DFS.java
@@ -203,3 +203,194 @@ class Trie {
 }
 
 __________________________________________________________________________________________________
+
+
+Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+
+A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+
+
+class Solution {
+    
+    public List<String> letterCombinations(String digits) {
+        List<String> ans = new ArrayList<>();
+        List<String> adj = List.of("abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz");
+        recur(digits, 0, adj, new StringBuilder(), ans);
+        return ans;
+    }
+
+    private void recur(String digits, int i, List<String> adj, StringBuilder temp, List<String> ans) {
+        if (i == digits.length()) {
+            ans.add(temp.toString());
+            return;
+        }
+        int d = Integer.parseInt(""+digits.charAt(i));
+        for(char j : adj.get(d-2).toCharArray()) {
+            temp.append(j);
+            recur(digits, i+1, adj, temp, ans);
+            temp.deleteCharAt(temp.length()-1);
+        }
+    }
+}
+
+
+TC : o(4^n)
+PATTERN : Backtracking
+MISTAKE: failed to delete char. SO basically when recursion come back to previous stack it was refering to temp String which was updated by nextforward stacks
+        Resulting in error. You can use final result without backtracking.
+
+
+
+long fastPow(long a, long n) {
+    long result = 1;
+
+    while (n > 0) {
+
+        if ((n & 1) == 1) {   // if n is odd
+            result *= a;
+        }
+
+        a = a * a;   // square
+        n = n >> 1;  // divide by 2
+    }
+
+    return result;
+}
+| Complexity     | Max N that usually passes |
+| -------------- | ------------------------- |
+| **O(1)**       | always                    |
+| **O(log N)**   | up to (10^{18})           |
+| **O(N)**       | (10^7 – 10^8)             |
+| **O(N log N)** | (10^6 – 10^7)             |
+| **O(N²)**      | (10^3 – 10^4)             |
+| **O(N³)**      | (300 – 500)               |
+| **O(2ⁿ)**      | (n ≤ 20)                  |
+| **O(N!)**      | (n ≤ 10)                  |
+
+______________________________________________________________________________________________________________________________________
+
+Given an integer array nums and an integer k, return the kth largest element in the array.
+
+Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i = 0; i < nums.length; i++) {
+            pq.add(nums[i]);
+            if (pq.size() > k) pq.poll();
+        }
+        return pq.poll();
+
+    }
+}
+
+Time Complexity: O(nlogk)
+Each of the n elements is processed once. However, heap operations take O(logk) time, leading to an overall complexity of O(nlogk).
+
+Space Complexity: O(k)
+The solution uses a heap with a maximum of k elements.
+PATTERN: if you want largest then take minheap and remove if getting full since you are removing smallest
+
+(To - DO How priority Queue structure uses ? HeapSort ? And sorting algo's like quicksort)
+__________________________________________________________________________________________________
+
+105. Construct Binary Tree from Preorder and Inorder Traversal
+Medium
+Topics
+premium lock icon
+Companies
+Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
+
+ 
+
+Example 1:
+
+
+Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+Output: [3,9,20,null,null,15,7]
+
+
+class Solution {
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return recur(preorder, 0, preorder.length - 1,
+                     inorder, 0, inorder.length - 1);
+    }
+
+    private TreeNode recur(int[] p, int pStart, int pEnd,
+                           int[] in, int iStart, int iEnd) {
+
+        if (pStart > pEnd || iStart > iEnd)
+            return null;
+
+        TreeNode root = new TreeNode(p[pStart]);
+
+        int mid = iStart;
+
+        for (int k = iStart; k <= iEnd; k++) {
+            if (in[k] == p[pStart]) {
+                mid = k;
+                break;
+            }
+        }
+
+        int leftSize = mid - iStart;
+
+        root.left = recur(p, pStart + 1, pStart + leftSize,
+                          in, iStart, mid - 1);
+
+        root.right = recur(p, pStart + leftSize + 1, pEnd,
+                           in, mid + 1, iEnd);
+
+        return root;
+    }
+}
+
+Current code:
+
+Time  : O(n²)
+Space : O(n)
+
+Optimized with HashMap:
+
+Time : O(n)
+
+
+
+MISTAKE: Approach was correct but failed to implement. Need extra care & choose early whether to take o or 1 based index. Failed to
+    honor recursion pass by reference and root
+
+    in recursion I was like root.left = recur(); but return type was void. Also created new Root() before recur call.
+
+
+Optimized
+class Solution {
+    private int preorderIndex;
+    private Map<Integer, Integer> mapping;
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        mapping = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) {
+            mapping.put(inorder[i], i);
+        }
+
+        preorderIndex = 0;
+        return build(preorder, 0, inorder.length - 1);        
+    }
+
+    private TreeNode build(int[] preorder, int start, int end) {
+        if (start > end) return null;
+
+        int rootVal = preorder[preorderIndex++];
+        TreeNode root = new TreeNode(rootVal);
+        int mid = mapping.get(rootVal);
+
+        root.left = build(preorder, start, mid - 1);
+        root.right = build(preorder, mid + 1, end);
+
+        return root;
+    }
+}
+______________________________________________________________________________________________________________________________________________
